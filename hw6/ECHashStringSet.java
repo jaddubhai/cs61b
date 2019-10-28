31f605a0463b86702c1509f546ea98d16176644f
Varun Jadia <jvarun@berkeley.edu>
2019-10-27 19:54:42 -0700

    updated hw6

--- hw6/ECHashStringSet.java
+++ hw6/ECHashStringSet.java
@@ -1,21 +1,77 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 /** A set of String values.
  *  @author
  */
 class ECHashStringSet implements StringSet {
+
+    public ECHashStringSet() {
+
+        _HashMap = new LinkedList[5];
+        for (int i = 0; i < 5; i++) {
+            _HashMap[i] = new LinkedList<String>();
+        }
+    }
+
     @Override
     public void put(String s) {
-        // FIXME
+        if (load() > _maxload) {
+            List vals = asList();
+            _HashMap = new LinkedList[2*_HashMap.length];
+
+            for (Object elem : vals) {
+                int idx = (elem.hashCode() & 0x7fffffff) %_HashMap.length;
+                _HashMap[idx].add((String) elem);
+            }
+        }
+
+        int idx;
+        if (_HashMap == null) {
+             idx = (s.hashCode() & 0x7fffffff);
+        } else {
+             idx = (s.hashCode() & 0x7fffffff) % _HashMap.length;
+        }
+
+        _HashMap[idx].add(s);
     }
 
     @Override
     public boolean contains(String s) {
-        return false; // FIXME
+        int idx;
+        if (_HashMap == null) {
+            idx = (s.hashCode() & 0x7fffffff);
+        } else {
+            idx = (s.hashCode() & 0x7fffffff) % _HashMap.length;
+        }
+
+        for (String item: _HashMap[idx]) {
+            if (item.equals(s)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public List<String> asList() {
-        return null; // FIXME
+        LinkedList<String> retarr = new LinkedList<String>();
+        for (LinkedList<String> strlst : _HashMap) {
+            retarr.addAll(strlst);
+        }
+        return  retarr;
     }
+
+    private int load() {
+        if (_HashMap == null) {
+            return 0;
+        }
+        return _HashMap[0].size()/_HashMap.length;
+    }
+
+    private LinkedList<String>[] _HashMap;
+    private final int _minload = 0/2;
+    private final int _maxload = 5;
 }
